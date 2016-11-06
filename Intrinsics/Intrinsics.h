8b3d8465391ad7ed5af70b6e25d3ec4d673009d7
ysnrkdm <euphonictech@gmail.com>
2016-11-06 15:41:02 +0900

    Move Intrinsics to separate dir

---
+++ Intrinsics/Intrinsics.h
@@ -0,0 +1,19 @@
+//
+//  Intrinsics.h
+//  Intrinsics
+//
+//  Created by KodamaYoshinori on 2016/11/06.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Intrinsics.
+FOUNDATION_EXPORT double IntrinsicsVersionNumber;
+
+//! Project version string for Intrinsics.
+FOUNDATION_EXPORT const unsigned char IntrinsicsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Intrinsics/PublicHeader.h>
+
+
---
+++ Sources/Intrinsics/intrinsics.h
@@ -0,0 +1,17 @@
+//
+//  intrinsics.h
+//  Graphene
+//
+//  Created by Kodama Yoshinori on 12/24/14.
+//  Copyright (c) 2014 Yoshinori Kodama. All rights reserved.
+//
+
+#ifndef __Graphene__intrinsics__
+#define __Graphene__intrinsics__
+
+#include <stdio.h>
+
+extern unsigned int _bitScanForward(uint64_t board);
+extern unsigned int _bitPop(uint64_t board);
+
+#endif /* defined(__Graphene__intrinsics__) */
